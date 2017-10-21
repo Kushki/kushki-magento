adddd3da7a22467f66deba467dbfe1e6093038f0
Patricio Moreano <pmoreanoj@gmail.com>
2017-10-21 17:50:48 -0500

    [KV-2057] Logic and test dir

---
+++ Kushki/Kushki/KushkiPayment/tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patricio
+ * Date: 10/21/17
+ * Time: 5:43 PM
+ */
+
+use PHPUnit_Framework_TestCase;
+
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+
+}
