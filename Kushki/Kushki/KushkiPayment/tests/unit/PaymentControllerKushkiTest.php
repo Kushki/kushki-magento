1a9606e8ea9bfcbeee740dafb2d0eaa79fa76785
moransk8 <davidm@kushkipagos.com>
2017-10-24 19:54:39 -0500

    [KV-2058]

--- Kushki/Kushki/KushkiPayment/tests/unit/PaymentControllerKushkiTest.php
+++ Kushki/Kushki/KushkiPayment/tests/unit/PaymentControllerKushkiTest.php
@@ -6,9 +6,9 @@
  * Time: 5:02 PM
  */
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
-class PaymentControllerKushkiTest extends PHPUnit_Framework_TestCase
+class PaymentControllerKushkiTest extends PHPUnit\Framework\TestCase
 {
     public function testGetTaxDetails() {
         $expectedResult = array(
@@ -19,11 +19,44 @@ class PaymentControllerKushkiTest extends PHPUnit_Framework_TestCase
                 'quantity' => '1',
                 'tax' => ['4', '6']
             ]);
-
-        $stub = $this->createMock(Kushki_KushkiPayment_PaymentControllerKushki::class);
+        $stub = $this->createMock(PaymentControllerKushki::class);
         $stub->method('getTaxDetails')
             ->willReturn($expectedResult);
-        $this->assertEquals($expectedResult, $stub->getTaxDetails());
+        $this->assertEquals($expectedResult, $stub->getTaxDetails(), 'Tax details not equal');
     }
 
+    public function testGetProducts(){
+        $expectedResult = array(['producto1'],['producto2'], ['producto3']);
+        $stub = $this->createMock(PaymentControllerKushki::class);
+        $stub->method('getProducts')
+            ->willReturn($expectedResult);
+        $this->assertEquals($expectedResult, $stub->getProducts(), 'Products not equal');
+    }
+
+    public function testGetTaxAmount(){
+        $taxDetails = array(
+            [
+                'productId' => '1',
+                'price' => '280',
+                'totalTax' => '12',
+                'quantity' => '1',
+                'tax' => ['4', '6']
+            ]);
+        $amount = [
+            'subtotalIva' => 0,
+            'subtotalIva0' => 0,
+            'iva' => 0,
+            'ice' => 0,
+            'extraTaxes' => [
+                'propina' => 0,
+                'tasaAeroportuaria' => 0,
+                'agenciaDeViaje' => 0,
+                'iac' => 0
+            ]
+        ];
+        $stub = $this->createMock(PaymentControllerKushki::class);
+        $stub->method('getTaxAmount')
+            ->willReturn($amount);
+        $this->assertEquals($amount, $stub->getTaxAmount($taxDetails), 'Amount not equal');
+    }
 }
