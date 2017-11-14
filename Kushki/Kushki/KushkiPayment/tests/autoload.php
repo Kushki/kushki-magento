b1dc2aad614fae01ddaf33f3fd0528e0282a712b
Patricio Moreano <pmoreanoj@gmail.com>
2017-11-14 15:12:25 -0500

    [KV-0] New version

--- Kushki/Kushki/KushkiPayment/tests/autoload.php
+++ Kushki/Kushki/KushkiPayment/tests/autoload.php
@@ -1,3 +1,7 @@
 <?php
 
+<<<<<<< HEAD
+require_once(dirname(__FILE__) . '/../controllers/PaymentControllerKushki.php');
+=======
 require_once(dirname(__FILE__) . '/../controllers/Paymentcontrollerkushki.php');
+>>>>>>> [KV-0] New version
